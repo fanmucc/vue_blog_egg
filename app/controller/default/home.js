09e5e8c62b2e78eefdbe31de995ddf92f678a49f
fanmu <fanmucc@yeah.net>
2020-08-26 23:20:48 +0800

    路由接口配置，前后端接口分离

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -1,12 +1,17 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-
+// restful架构
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
     ctx.body = 'hi, eggs123';
   }
+  async list () {
+    const { ctx } = this;
+    ctx.body = ctx
+  }
 }
+
 
 module.exports = HomeController;
